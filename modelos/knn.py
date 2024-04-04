4cc0fd843f1c313bd3d9eea2b2d0c0b71b3ed511
Pedro Felix <14181939+pedronet007@users.noreply.github.com>
2024-04-04 20:09:41 -0300

    Update knn.py

--- modelos/knn.py
+++ modelos/knn.py
@@ -113,82 +113,91 @@ def plot_decision_surface(X, y, classifier, resolution=0.02):
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=[cmap(idx)], marker=markers[idx], label=cl)
 
-
 # Realizando 20 interações
 def cross_validation (k,X,y):
-	n_realizations = 20
-	accuracies = []
-	for i in range(n_realizations):
-		# Dividindo os dados em conjuntos de treinamento e teste
-		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=i)
+    n_realizations = 20
+    accuracies = []
+    for i in range(n_realizations):
+        # Dividindo os dados em conjuntos de treinamento e teste
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=i)
+        # Normalizando os dados (opcional)
+        # scaler = StandardScaler()
+        # X_train = scaler.fit_transform(X_train)
+        # X_test = scaler.transform(X_test)
+        # Treinando o modelo KNN
+        knn = KNN(k=k)
+        knn.fit(X_train, y_train)
 
-		# Normalizando os dados (opcional)
-		# scaler = StandardScaler()
-		# X_train = scaler.fit_transform(X_train)
-		# X_test = scaler.transform(X_test)
+        # Fazendo previsões
+        y_pred = knn.predict(X_test)
 
-		# Treinando o modelo KNN
-		knn = KNN(k=k)
-		knn.fit(X_train, y_train)
-
-		# Fazendo previsões
-		y_pred = knn.predict(X_test)
-
-		# Calculando a acurácia
-		accuracy = accuracy_score(y_test, y_pred)
-		accuracies.append(accuracy)
-		#conf_matrix = confusion_matrix(y_test, y_pred, num_classes=len(np.unique(y)))
-		#print(f"Matriz de confusão para a realização {i}:\n{conf_matrix}")
-
-	# Calculando média e desvio padrão da acurácia
-	mean_accuracy = np.mean(accuracies)
-	#Desvio padrao
-	std_accuracy = np.std(accuracies)
-
-	print(f"Acurácia média: {mean_accuracy}")
-	print(f"Desvio padrão da acurácia: {std_accuracy}")
+        # Calculando a acurácia
+        accuracy = accuracy_score(y_test, y_pred)
+        accuracies.append(accuracy)
+        #conf_matrix = confusion_matrix(y_test, y_pred, num_classes=len(np.unique(y)))
+        #print(f"Matriz de confusão para a realização {i}:\n{conf_matrix}")
+    # Calculando média e desvio padrão da acurácia
+    mean_accuracy = np.mean(accuracies)
+    #Desvio padrao
+    std_accuracy = np.std(accuracies)
+    print(f"Acurácia média: {mean_accuracy}")
+    print(f"Desvio padrão da acurácia: {std_accuracy}")
 
 #Chamando o KNN e passando os atributos com seus rótulos
 def run(X,y,colunas,classes):
-	# Defina a faixa de valores de K que você deseja testar
-	k_range = range(1, 21)  # Testando de 1 a 20 vizinhos
-	random_realization = 42 #Numero 42 é mágico mais utilizado. rsrs
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_realization)
-	X_train = X_train[:, :2]
-	X_test = X_test[:, :2]
+    # Defina a faixa de valores de K que você deseja testar
+    k_range = range(1, 21)  # Testando de 1 a 20 vizinhos
+    random_realization = 4 #Numero 42 é mágico mais utilizado. rsrs
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_realization)
+    #Alteracao que fixava axis do X_train em duas colunas
+    #X_train = X_train[:, :2]
+    #X_test = X_test[:, :2]
 	# Chame a função knee_curve para plotar a curva do joelho para encontrar o melhor valor de K
-	df_scores = knee_curve(X_train, y_train, k_range, X_test, y_test)
+    df_scores = knee_curve(X_train, y_train, k_range, X_test, y_test)
     #Escolha do melhor K a partir da ordenação da maior acuracia pegando seu maior valor do dicionario criado com a lista de K e acuracias a partir da curva do joelho
-	scores = df_scores["ACCURACY"].tolist()
-	k_optimal = df_scores.sort_values("ACCURACY", ascending=False).head(1)["K"].values[0]
-	print("Optimal K", k_optimal) 
-	knee_plot_curve(scores,k_range)
-	print(df_scores)
-	knn = KNN(k=k_optimal)
-	knn.fit(X_train, y_train)
-	y_pred = knn.predict(X_test)
-	conf_matrix = confusion_matrix(y_test, y_pred, num_classes=len(np.unique(y)))
-	print(f"Matriz de confusão para a realização {random_realization}:\n{conf_matrix}")
+    scores = df_scores["ACCURACY"].tolist()
+    k_optimal = df_scores.sort_values("ACCURACY", ascending=False).head(1)["K"].values[0]
+    print("Optimal K", k_optimal) 
+    knee_plot_curve(scores,k_range)
+    print(df_scores)
+    knn = KNN(k=k_optimal)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_pred, num_classes=len(np.unique(y)))
+    print(f"Matriz de confusão com o melhor K para a realização {random_realization}:\n{conf_matrix}")
 
 	# Escolhendo um par de atributos aleatório para plotar a superfície de decisão
 	# print(X_train[:, :2].shape, X_train_subset.shape)
-	plt.figure(figsize=(10, 6))
-	plot_decision_surface(X_train, y_train, knn)
-	plt.xlabel(colunas[0])
-	plt.ylabel(colunas[1])
-	plt.title('Superfície de Decisão para os Atributos Selecionados')
-	plt.legend(loc='upper left')
+    #plt.figure(figsize=(10, 6))
+    #plot_decision_surface(X_train, y_train, knn)
+    #plt.xlabel(colunas[0])
+    #plt.ylabel(colunas[1])
+    #plt.title('Superfície de Decisão para os Atributos Selecionados')
+    #plt.legend(loc='upper left')
+
+    # Escolhendo um par de atributos aleatório para plotar a superfície de decisão
+    random_features = np.random.choice(range(X.shape[1]), size=2, replace=False)
+    X_train_subset = X_train[:, random_features]
+    X_test_subset = X_test[:, random_features]
+    knn.fit(X_train_subset, y_train)
+
+    plt.figure(figsize=(10, 6))
+    plot_decision_surface(X_train_subset, y_train, knn)
+    plt.xlabel(colunas[random_features[0]])
+    plt.ylabel(colunas[random_features[1]])
+    plt.title('Superfície de Decisão para os Atributos Selecionados')
+    plt.legend(loc='upper left')
 
 	## plot da matriz de confusão com o seaborn
 
-	plt.figure(figsize=(10,6))
-	fx=sns.heatmap(conf_matrix, annot=True, fmt=".2f",cmap="GnBu")
-	fx.set_title('Confusion Matrix \n');
-	fx.set_xlabel('\n Predicted Values\n')
-	fx.set_ylabel('Actual Values\n');
-	fx.xaxis.set_ticklabels(classes)
-	fx.yaxis.set_ticklabels(classes)
-	plt.show()
+    plt.figure(figsize=(10,6))
+    fx=sns.heatmap(conf_matrix, annot=True, fmt=".2f",cmap="GnBu")
+    fx.set_title('Confusion Matrix \n');
+    fx.set_xlabel('\n Predicted Values\n')
+    fx.set_ylabel('Actual Values\n');
+    fx.xaxis.set_ticklabels(classes)
+    fx.yaxis.set_ticklabels(classes)
+    plt.show()
 
-	#Chamada das 20 realizações crosvalidando e verificando a acuracia 
-	cross_validation(k_optimal, X,y)
+    #Chamada das 20 realizações crosvalidando e verificando a acuracia 
+    cross_validation(k_optimal, X,y)
