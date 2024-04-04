c2ba9da5a4eb0240a7b0c8087cc38e133dc0a848
Pedro Felix <pedro.felix@outlook.com>
2024-04-04 10:10:57 -0300

    Add files via upload

---
+++ run_knn_artificial.py
@@ -0,0 +1,18 @@
+from knn import *
+
+# Carregando os dados
+caminho = "./dados/artificial.data"
+colunas = ['a', 'b', 'class']
+dataset = pd.read_csv(caminho, names=colunas)
+
+# Mapeando os rótulos de classe para valores numéricos
+class_mapping = {label: idx for idx, label in enumerate(np.unique(dataset['class']))}
+classes = dataset["class"].unique()
+dataset['class'] = dataset['class'].map(class_mapping)
+
+# Dividindo os dados em atributos e rótulos
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+run(X,y,colunas,classes)
