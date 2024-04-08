6183a751140b2e0fca1b6954a9f4f9c1c1c8285c
Pedro Felix <14181939+pedronet007@users.noreply.github.com>
2024-04-08 16:40:37 -0300

    Update gaussian_bayesian_classifier.py

--- modelos/gaussian_bayesian_classifier.py
+++ modelos/gaussian_bayesian_classifier.py
@@ -19,13 +19,39 @@ def posterior_probability(x, mean, covariance, prior):
     denominator = np.sqrt((2 * np.pi) ** dimension * np.linalg.det(covariance))
     return prior * np.exp(exponent) / denominator
 
+def train(X, y):
+    classes = np.unique(y)
+    means = {}
+    covariances = {}
+    priors = {}
+    for c in classes:
+        X_c = X[y == c]
+        means[c] = calculate_mean(X_c)
+        covariances[c] = calculate_covariance(X_c)
+        priors[c] = len(X_c) / len(X)
+    return means, covariances, priors
+
+def classify(x, means, covariances, priors):
+    classes = list(means.keys())
+    probabilities = [posterior_probability(x, means[c], covariances[c], priors[c]) for c in classes]
+    return classes[np.argmax(probabilities)]
+
+#comentar o original
+#if __name__ == "__main__":
+#    # Example usage:
+#    X = np.array([[1, 2], [2, 3], [3, 4]])
+#    mean = calculate_mean(X)
+#    covariance = calculate_covariance(X)
+#    x = np.array([2, 3])
+#    prior = 1/3
+#    prob = posterior_probability(x, mean, covariance, prior)
+#    print(prob)
+
 
 if __name__ == "__main__":
-    # Example usage:
-    X = np.array([[1, 2], [2, 3], [3, 4]])
-    mean = calculate_mean(X)
-    covariance = calculate_covariance(X)
+    # Exemplo de uso:
+    X = np.array([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6]])
+    y = np.array([0, 0, 1, 1, 1])
+    means, covariances, priors = train(X, y)
     x = np.array([2, 3])
-    prior = 1/3
-    prob = posterior_probability(x, mean, covariance, prior)
-    print(prob)
+    print(classify(x, means, covariances, priors))
