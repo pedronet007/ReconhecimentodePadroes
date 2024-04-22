d6c963160b3cb632861a553c8ae9636cb01e4d14
Pedro Felix <14181939+pedronet007@users.noreply.github.com>
2024-04-21 23:23:22 -0300

    Add files via upload

--- modelos/cnb.py
+++ modelos/cnb.py
@@ -3,79 +3,164 @@ from collections import defaultdict
 #ALUNO PEDRO WILSON FELIX M NETO KNN - 2024.1
 # Criacao do Classificador CNB
 
-
-class NaiveBayes:
+class NaiveBayesClassifier:
     def fit(self, X, y):
         self.classes = np.unique(y)
-        self.class_priors = self.compute_class_priors(y)
-        self.means, self.stds = self.compute_class_stats(X, y)
-    
+        self.class_probs = np.zeros(len(self.classes))
+        self.feature_probs = []
+        for c in self.classes:
+            X_c = X[y == c]
+            idx = np.where(self.classes == c)[0][0]  # Obter o índice do elemento c em self.classes
+            self.class_probs[idx] = len(X_c) / len(X)
+            feature_probs_c = []
+            for feature in range(X.shape[1]):
+                unique_values, counts = np.unique(X_c[:, feature], return_counts=True)
+                feature_probs_c.append({val: (counts[i] + 1) / (len(X_c) + len(unique_values)) for i, val in enumerate(unique_values)})
+            self.feature_probs.append(feature_probs_c)
+
     def predict(self, X):
-        predictions = []
+        preds = []
         for x in X:
-            posterior_probs = self.calculate_posterior_probs(x)
-            predictions.append(np.argmax(posterior_probs))
-        return predictions
-    
-    def compute_class_priors(self, y):
-        class_counts = defaultdict(int)
-        for label in y:
-            class_counts[label] += 1
-        return {label: count / len(y) for label, count in class_counts.items()}
-    
-    def compute_class_stats(self, X, y):
-        means = {}
-        stds = {}
-        for label in self.classes:
-            X_class = X[y == label]
-            means[label] = np.mean(X_class, axis=0)
-            stds[label] = np.std(X_class, axis=0)
-        return means, stds
-    
-    def calculate_likelihood(self, x, mean, std):
-        exponent = -0.5 * ((x - mean) / std) ** 2
-        return np.exp(exponent) / (np.sqrt(2 * np.pi) * std)
-    
-    def calculate_posterior_probs(self, x):
-        posterior_probs = {}
-        for label in self.classes:
-            class_prior = self.class_priors[label]
-            likelihoods = self.calculate_likelihood(x, self.means[label], self.stds[label])
-            posterior_probs[label] = np.prod(likelihoods) * class_prior
-        return posterior_probs
-    
-    def plot_gaussians(self, colunas):
-        num_classes = len(self.classes)
-        num_features = len(self.means[self.classes[0]])
-        plt.figure(figsize=(12, 8))
+            class_probs_x = []
+            for idx, c in enumerate(self.classes):
+                idx = np.where(self.classes == c)[0][0]  # Obter o índice do elemento c em self.classes
+                class_prob = np.log(self.class_probs[idx])
+                for feature, val in enumerate(x):
+                    if val in self.feature_probs[idx][feature]:
+                        class_prob += np.log(self.feature_probs[idx][feature][val])
+                class_probs_x.append(class_prob)
+            preds.append(np.argmax(class_probs_x))
+        return preds
 
-        for i, label in enumerate(self.classes):
-            plt.subplot(1, num_classes, i + 1)
-            plt.title(f"Classe {label}")
-            for j in range(num_features):
-                mean = self.means[label][j]
-                std = self.stds[label][j]
-                x_values = np.linspace(mean - 3 * std, mean + 3 * std, 100)
-                y_values = self.calculate_likelihood(x_values, mean, std)
-                plt.plot(x_values, y_values, label=colunas[j])
-                plt.xlabel("Valor")
-                plt.ylabel("Densidade de Probabilidade")
+def confusion_matrix(y_true, y_pred, num_classes):
+    matrix = np.zeros((num_classes, num_classes), dtype=int)
+    for true_label, pred_label in zip(y_true, y_pred):
+        matrix[int(true_label), int(pred_label)] += 1
+    return matrix
 
-        plt.legend()
-        plt.tight_layout()
-        plt.show()
+def calculate_accuracy(y_true, y_pred):
+    return np.mean(y_true == y_pred)
 
+def run_multiple_iterations(X, y, num_iterations=20, test_size=0.2, random_state=None):
+    accuracies = np.zeros(num_iterations)
+    for i in range(num_iterations):
+        # Dividindo os dados em treino e teste
+        X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_size, random_state=i)
+
+        # Criando e treinando o classificador Naive Bayes
+        nb_classifier = NaiveBayesClassifier()
+        nb_classifier.fit(X_train, y_train)
+
+        # Fazendo previsões
+        y_pred = nb_classifier.predict(X_test)
+
+        # Calculando a acurácia
+        accuracy = calculate_accuracy(y_test, y_pred)
+
+        # Armazenando a acurácia
+        accuracies[i] = accuracy
+    # Calculando a média e o desvio padrão das acurácias
+    mean_accuracy = np.mean(accuracies)
+    std_accuracy = np.std(accuracies)
+    # Exibindo os resultados
+    print("Acurácias:")
+    print(accuracies)
+    print(f"Acurácia média: {mean_accuracy:.2f}")
+    print(f"Desvio padrão: {std_accuracy:.2f}")
+    return accuracies
+
+def plot_gaussians(X, y, classes, attribute1_index, attribute2_index, colunas):
+    plt.figure(figsize=(10, 8))
+    for idx, class_name in enumerate(classes):
+        class_data = X[y == idx]
+        mean = np.mean(class_data, axis=0)
+        # Convertendo a lista de valores para um array NumPy
+        class_data_array = np.array(class_data)
+        # Calculando a matriz de covariância
+        cov = np.cov(class_data_array.T)
+        #cov = np.cov(class_data.T)
+        samples = np.random.multivariate_normal(mean, cov, 1000)
+        plt.scatter(samples[:, attribute1_index], samples[:, attribute2_index], label=f'{class_name} gaussian', alpha=0.2)
+    # Configurar o plot
+    plt.xlabel(f'Feature {colunas[attribute1_index]}')
+    plt.ylabel(f'Feature {colunas[attribute2_index]}')
+    plt.title('Gaussian Distributions')
+    plt.legend()
+    plt.show()
+
+def plot_decision_surface_cnb(X, y, classifier, classes, attribute1_index, attribute2_index, colunas):
+    h = .02  
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+    cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+    Z = np.array(Z)  # Convertendo para um array numpy
+    Z = Z.reshape(xx.shape)
+    plt.figure(figsize=(10, 8))
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+    for idx, class_name in enumerate(classes):
+        plt.scatter(X[y == idx][:, attribute1_index], X[y == idx][:, attribute2_index], edgecolor='k', s=20, label=class_name)
+
+    # Configurar o plot
+    plt.xlabel(f'Feature {colunas[attribute1_index]}')
+    plt.ylabel(f'Feature {colunas[attribute2_index]}')
+    plt.title('Decision Surface')
+    plt.legend()
+    plt.show()
 
 def run(X,y,colunas,classes):
-    random_realization=42
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_realization)
+    # Definindo configurações para as realizações
+    num_realizacoes = 20
+    test_size = 0.2   # Separando 80% para treinamento e 20% para teste
+    random_realization = 42   
+    # Dividindo os dados em treino e teste
+    X_train, X_test, y_train, y_test = split_data(X, y, test_size=test_size, random_state=random_realization)
 
-    # Treinar o modelo
-    nb = NaiveBayes()
-    nb.fit(X_train, y_train)
-    # Fazer previsões
-    predictions = nb.predict(X_test)
-    accuracy = np.mean(predictions == y_test)
-    print("Acurácia:", accuracy)
-    # Plotar as distribuições gaussianas
-    nb.plot_gaussians(colunas=colunas)
+    # Criando e treinando o classificador Naive Bayes
+    nb_classifier = NaiveBayesClassifier()
+    nb_classifier.fit(X_train, y_train)
+
+    # Chamando a função para executar as 20 realizações
+    accuracies = run_multiple_iterations(X, y, num_iterations=num_realizacoes, test_size=test_size, random_state=random_realization)
+    # Encontrar a realização com a melhor acurácia
+    best_accuracy_index = np.argmax(accuracies)
+    best_accuracy = accuracies[best_accuracy_index]
+    print("Melhor acurácia ao utilizar randon_state {}, acontece na (Realização {}): {:.2f}".format(random_realization,best_accuracy_index + 1, best_accuracy))
+
+
+    # Escolher um par de atributos para a superfície de decisão sepal-width e sepal-length
+    attribute1_index = 0
+    attribute2_index = 1
+    # Dados de treinamento e teste com os atributos escolhidos
+    X_train_selected = X_train[:, [attribute1_index, attribute2_index]]
+    X_test_selected = X_test[:, [attribute1_index, attribute2_index]]
+    print("Dados de Treinamento:")
+    dados_com_labels = np.column_stack((X_train,y_train.astype(int)))
+    print (dados_com_labels)
+    print("Dados de teste:")
+    dados_com_labels_teste = np.column_stack((X_test, y_test.astype(int)))
+    print (dados_com_labels_teste)
+
+    # Fazendo previsões
+    y_pred = nb_classifier.predict(X_test)
+
+    # Calculando a matriz de confusão
+    conf_matrix = confusion_matrix(y_test, y_pred, num_classes=len(np.unique(y)))
+    # Exibindo a matriz de confusão
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, cmap="Blues", fmt="d", xticklabels=classes, yticklabels=classes)
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+    # Plotando a superfície de decisão
+    plot_decision_surface_cnb(X_train, y_train, nb_classifier, classes, attribute1_index, attribute2_index, colunas)
+    # Plotando as gaussianas
+    plot_gaussians(X_train, y_train, classes, attribute1_index, attribute2_index, colunas)
+    # Plotar os conjuntos de dados de treinamento e teste
+    plot_dataset(X_train_selected, y_train, X_test_selected, y_test, colunas, attribute1_index, attribute2_index)
